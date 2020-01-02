94b75c116da39f8975aa1af434bc47d3586870e9
Brian Voong <bhlvoong@gmail.com>
2020-01-02 08:45:48 -0800

    Initial Commit

---
+++ CalculatorSwiftUILBTA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculatorSwiftUILBTA
+//
+//  Created by Brian Voong on 1/2/20.
+//  Copyright © 2020 Lets Build That App. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
